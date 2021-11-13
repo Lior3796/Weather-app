732c566b7b1380458346e2c788a3c22aa4c479c1
Lior3796 <liorlsa9@gmail.com>
2021-11-14 00:05:58 +0200

    replaced api-key

--- src/components/service/service.js
+++ src/components/service/service.js
@@ -1,18 +1,18 @@
-
+const modal = require("react-toastify");
 export const getCityList = (city) => {
-    return fetch(`http://dataservice.accuweather.com//locations/v1/cities/autocomplete?apikey=4jiV4kQw8qp9aKj7rTVgSPBcJJSGQ5zJ&q=${city}&language=en-us HTTP/1.1`)
+    return fetch(`https://dataservice.accuweather.com//locations/v1/cities/autocomplete?apikey=${process.env.REACT_APP_API_KEY}&q=${city}&language=en-us HTTP/1.1`)
         .then(res => res.json())
 
 }
 
 export const getCityWeather = ({ Key }) => {
-    return fetch(`http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=4jiV4kQw8qp9aKj7rTVgSPBcJJSGQ5zJ&language=en-us&details=true`)
+    return fetch(`https://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${process.env.REACT_APP_API_KEY}&language=en-us&details=true`)
         .then(res => res.json())
 
 }
 
 export const getDailyForecasts = (Key) => {
-    return fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${Key}?apikey=4jiV4kQw8qp9aKj7rTVgSPBcJJSGQ5zJ`)
+    return fetch(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${Key}?apikey=${process.env.REACT_APP_API_KEY}`)
         .then(res => res.json())
 
 
